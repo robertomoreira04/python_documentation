580823cc613da56455268ec715b8d35100a48fb5
Roberto Moreira <robertodev04@gmail.com>
2025-08-28 14:13:22 -0300

    atualização

--- exercises/exercicio1.py
+++
@@ -1,6 +0,0 @@
-# exercício de antecessor e sucessor 
-
-user_number = int(input('Digite um número: '))
-
-print(f'O antecessor de {user_number} é {user_number - 1} e o sucessor é {user_number + 1}. ')
-
