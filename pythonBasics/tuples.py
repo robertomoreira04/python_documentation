ee930353d3540776e4047bbbc6fac3f621e9a2ee
Roberto Moreira <robertodev04@gmail.com>
2025-08-29 10:15:25 -0300

    tuplas em python

---
+++ pythonBasics/tuples.py
@@ -0,0 +1,12 @@
+# tuplas em python
+
+my_tuple = ("Roberto", "Desenvolvedor", "FastAPI")
+
+#Valores não são adicionados, removidos e ordenados
+
+print(my_tuple[0]) # busca o primeiro
+
+print(my_tuple[-1]) # busca o último da tupla 
+
+print(my_tuple.index("Roberto")) # recupera valor pelo indice 
+
