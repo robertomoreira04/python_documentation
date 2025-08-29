98712120de4d66e569af6ae85d73047b55ed5e81
Roberto Moreira <robertodev04@gmail.com>
2025-08-29 14:32:12 -0300

    while em python

---
+++ pythonBasics/while.py
@@ -0,0 +1,9 @@
+my_list = ["Roberto", "Desenvolvedor", "Python", "Django", "FastAPI"]
+
+# Inicializando o índice
+i = 0
+
+# Enquanto o índice for menor que o tamanho da lista
+while i < len(my_list):
+    print(my_list[i])
+    i += 1  # incrementa o índice a cada iteração
