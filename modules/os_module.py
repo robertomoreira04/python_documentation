0c26f357ea05666ef576acc1972bf1f5c66778f1
Roberto Moreira <robertodev04@gmail.com>
2025-08-30 12:22:03 -0300

    módulo os em python

---
+++ modules/os_module.py
@@ -0,0 +1,14 @@
+import os 
+
+print(os.getcwd()) # retorna a pasta atual
+
+print(os.listdir()) #lista os diretórios 
+
+os.system('ver') # mostra a versão do SO
+
+os.system('systeminfo') # configurações do sistema
+
+os.system('cls') # limpa a tela de busca, parecido com o "clear" no terminal
+
+os.environ.get('MINHA_VARIAVEL') # busca variável de ambiente previamente salva 
+
