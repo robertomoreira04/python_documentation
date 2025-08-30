c6064268e34924fe9b26fec6d8f0a5734a762a56
Roberto Moreira <robertodev04@gmail.com>
2025-08-30 11:24:37 -0300

    criando e acessando módulos em python

---
+++ modules/scripts.py
@@ -0,0 +1,9 @@
+# usando o módulo 
+
+import calc_module # importa todo o módulo
+from calc_module import division # importa algo específico
+
+print(calc_module.sum(1, 3))
+
+print(calc_module.division(10, 2))
+
