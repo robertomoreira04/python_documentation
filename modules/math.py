9d5f2f10da62f190ff61516131d33dfc63ecebbf
Roberto Moreira <robertodev04@gmail.com>
2025-09-01 16:02:36 -0300

    módulo math em python

---
+++ modules/math.py
@@ -0,0 +1,28 @@
+import math 
+
+print(math.pi) # traz o número Pi
+
+print(f"{math.pi:.2f}") # Número Pi em duas casas decimais
+
+print(math.pi) # traz o número de Euler
+
+print(f"{math.e:.2f}") # Número Euler em duas casas decimais
+
+number = 10.4 
+
+print(math.ceil(number)) # arredonda número para cima 
+print(math.floor(number)) # arredonda número para baixo
+
+second_number = 7 
+
+print(math.factorial(second_number)) # realiza o fatorial de um número
+
+print(math.pow(5,5)) # realiza a potência de um número
+
+print(math.sqrt(169)) # realiza a raiz quadrada de um número
+
+mdc = math.gcd(25, 100, 10) # busca o maior divisor comum entre números
+print(mdc)
+
+print(math.log(10)) # calcula o logaritmo de um número
+
