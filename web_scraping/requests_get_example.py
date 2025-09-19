c11709aceeb77c75eb65ab202104f86642a0e5f0
Roberto Moreira <robertodev04@gmail.com>
2025-09-19 10:37:29 -0300

    exemplo de get com requests

---
+++ web_scraping/requests_get_example.py
@@ -0,0 +1,10 @@
+# exemplo de get na API json placeholder
+
+import requests 
+
+url = 'https://jsonplaceholder.typicode.com/posts/1'
+
+response = requests.get(url)
+
+response_json = response.json()
+print(response_json)
