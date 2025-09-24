9ec050c101960957abc9400ae17b32eb3f99c1b0
Roberto Moreira <robertodev04@gmail.com>
2025-09-24 11:43:26 -0300

    pesquisa básica na amazon

--- selenium/searching_amazon.py
+++ selenium/searching_amazon.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time 
 
 browser = webdriver.Firefox()
 
@@ -11,6 +12,3 @@ browser.get('https://www.amazon.com.br')
 element = browser.find_element(By.ID, 'twotabsearchtextbox')
 element.send_keys('ps5')
 element.send_keys(Keys.ENTER)
-
-
-
